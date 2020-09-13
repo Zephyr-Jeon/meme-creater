0512c331211b833919fc8b0b000f974b54f1d8b8
Zephyr Jeon <rltjd1258@gmail.com>
2020-09-14 04:26:54 +0900

    Updates

---
+++ precache-manifest.e2625b2cf48f9d99b4e9fba9eafba572.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e043867e731b73cec7d3bc91484a95a",
+    "url": "/meme-creater/index.html"
+  },
+  {
+    "revision": "dceab7f149e81636d74f",
+    "url": "/meme-creater/static/css/main.a694c67f.chunk.css"
+  },
+  {
+    "revision": "8303d4c2e234d92ed58b",
+    "url": "/meme-creater/static/js/2.e888135c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/meme-creater/static/js/2.e888135c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dceab7f149e81636d74f",
+    "url": "/meme-creater/static/js/main.04533cbe.chunk.js"
+  },
+  {
+    "revision": "55349289b6b0c2ffc049",
+    "url": "/meme-creater/static/js/runtime-main.e9e1c73a.js"
+  }
+]);
